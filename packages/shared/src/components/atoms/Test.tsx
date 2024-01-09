0d3f3d9c3f39fa07bfc9f2da64d20612e56c63ed
Hoang Anh <lehoaa01@heiway.net>
2024-01-09 10:25:14 +0700

    Add existing project files to Git

---
+++ packages/shared/src/components/atoms/Test.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+export const TestComponent: React.FC = () => {
+  return (
+    <View>
+      <Text>TestComponent</Text>
+    </View>
+  );
+};
